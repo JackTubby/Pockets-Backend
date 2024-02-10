5ca905a2a80ac2ac479046431c22ea88e1ce0584
Jack Tubby <jack200034@hotmail.com>
2024-02-10 10:57:53 +0000

    Just temp add any types on routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { body, oneOf, validationResult } from "express-validator";
+// import { body, oneOf, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -7,19 +7,19 @@ const router = Router();
  * BANK_ACCOUNT
  */
 
-router.get('/bankaccount', (req, res) => {})
-router.get('/bankaccount/:id', (req, res) => {})
-router.put('/bankaccount/:id', (req, res) => {})
-router.post('/bankaccount', (req, res) => {})
-router.delete('/bankaccount', (req, res) => {})
+router.get('/bankaccount', (_req, _res) => {})
+router.get('/bankaccount/:id', (_req, _res) => {})
+router.put('/bankaccount/:id', (_req, _res) => {})
+router.post('/bankaccount', (_req, _res) => {})
+router.delete('/bankaccount', (_req, _res) => {})
 
 /**
  * Pocket
  */
-router.get('/pocket', (req, res) => {})
-router.get('/pocket/:id', (req, res) => {})
-router.put('/pocket/:id', (req, res) => {})
-router.post('/pocket', (req, res) => {})
-router.delete('/pocket', (req, res) => {})
+router.get('/pocket', (_req, _res) => {})
+router.get('/pocket/:id', (_req, _res) => {})
+router.put('/pocket/:id', (_req, _res) => {})
+router.post('/pocket', (_req, _res) => {})
+router.delete('/pocket', (_req, _res) => {})
 
 export default router
