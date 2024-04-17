b15345426cc58065fb9a6557ed219b4aa9fb5000
Jack Tubby <jack200034@hotmail.com>
2024-04-17 21:52:28 +0100

    Init user validation

---
+++ src/handlers/userValidation.ts
@@ -0,0 +1,8 @@
+// import { Request, Response, NextFunction } from "express";
+
+// export const userValidation = (req: Request, res: Response, next: NextFunction) => {
+//   console.log('response: ', req)
+//   next();
+// };
+
+export const userValidation = () => {}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import { signIn, signUp } from './handlers/user'
+import { userValidation } from './handlers/userValidation'
 import router from './router'
 import { protect } from './modules/auth'
 import cors from 'cors'
@@ -13,13 +14,12 @@ app.use(express.json()) // allows the client to send use JSON
 app.use(express.urlencoded({extended: true})) // allows client to add query string or params otherwise it treats everything as a string
 
 app.get('/', (req: any, res: any) => { // TODO: update types
-  console.log('hello world')
   res.status(200)
   res.json({message: 'hello there!'})
 })
 
 app.use('/api', protect, router)
-app.post('/signup', signUp)
+app.post('/signup', userValidation, signUp)
 app.post('/signin', signIn)
 
 export default app
