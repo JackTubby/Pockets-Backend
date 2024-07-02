c1319a1b64a49e1c3ff8975d93b8151809cf397b
Jack Tubby <jack200034@hotmail.com>
2024-07-02 20:57:33 +0100

    Format

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,54 +1,52 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 export const comparePassword = (password: any, hash: any) => {
-  return bcrypt.compare(password, hash);
-};
+  return bcrypt.compare(password, hash)
+}
 
 export const hashPassword = (password: any) => {
-  return bcrypt.hash(password, 5);
-};
+  return bcrypt.hash(password, 5)
+}
 
 export const createJWT = (user: any) => {
   if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET environment variable is not defined");
+    throw new Error('JWT_SECRET environment variable is not defined')
   }
-  const token = jwt.sign(
-    { id: user.id, username: user.username },
-    process.env.JWT_SECRET
-  );
+  const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET)
   // token is now a string
-  return token;
-};
+  return token
+}
 
 export const protect = (req: any, res: any, next: any) => {
-  const bearer = req.headers.authorization; // check for the auth header
+  const bearer = req.headers.authorization // check for the auth header
 
   if (!bearer) {
-    res.status(401);
-    res.json({ message: "not authorised" });
-    return;
+    res.status(401)
+    res.json({ message: 'not authorised' })
+    return
   }
-  // we split on space as the user sends up "'Bearer' 'Token'"
-  const [, token] = bearer.split(" ");
 
-  if (!token) {
-    res.status(401);
-    res.json({ message: "not valid token" });
-    return;
+  // we split on space as the user sends up "'Bearer' 'Token'"
+  // const [, token] = bearer.split(' ')
+
+  if (!bearer) {
+    res.status(401)
+    res.json({ message: 'not valid token' })
+    return
   }
 
   try {
     if (!process.env.JWT_SECRET) {
-      throw new Error("JWT_SECRET environment variable is not defined");
+      throw new Error('JWT_SECRET environment variable is not defined')
     }
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-    next();
+    const user = jwt.verify(bearer, process.env.JWT_SECRET)
+    req.user = user
+    next()
   } catch (e) {
-    console.error(e);
-    res.status(401);
-    res.json({ message: "not valid token" });
-    return;
+    console.error(e)
+    res.status(401)
+    res.json({ message: 'not valid token' })
+    return
   }
-};
+}
