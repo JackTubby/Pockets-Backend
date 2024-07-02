27a4cf1464e7f72ade31a9d399ae6e3d638af09a
Jack Tubby <jack200034@hotmail.com>
2024-07-02 22:17:24 +0100

    Imrpove protect functionality

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -28,19 +28,17 @@ export const protect = (req: any, res: any, next: any) => {
   }
 
   // we split on space as the user sends up "'Bearer' 'Token'"
-  // const [, token] = bearer.split(' ')
+  const [type, token] = bearer.split(' ');
 
-  if (!bearer) {
-    res.status(401)
-    res.json({ message: 'not valid token' })
-    return
+  if (type !== 'Bearer' || !token) {
+    return res.status(401).json({ message: 'Not valid token' });
   }
 
   try {
     if (!process.env.JWT_SECRET) {
       throw new Error('JWT_SECRET environment variable is not defined')
     }
-    const user = jwt.verify(bearer, process.env.JWT_SECRET)
+    const user = jwt.verify(token, process.env.JWT_SECRET)
     req.user = user
     next()
   } catch (e) {
