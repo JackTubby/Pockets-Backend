45829d053cb6eb186c78da655989831d1664c60e
Jack Tubby <jack200034@hotmail.com>
2024-09-08 10:05:32 +0100

    Update JWT expire date

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -14,7 +14,7 @@ export const createJWT = (user: any) => {
     throw new Error('JWT_SECRET environment variable is not defined')
   }
   const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, {
-    expiresIn: '7d',
+    expiresIn: "30d",
   })
   // token is now a string
   return token
